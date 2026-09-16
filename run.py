9101258073829ee14f78292f136de7fdc626e261
Alexey Karachentsev <alexey.karachentsev@vniira.spb.su>
2026-09-16 09:56:21 +0300

    worked with run.py

--- run.py
+++ run.py
@@ -3,3 +3,24 @@ import asyncio
 import os
 import configparser
 import logging
+import sys
+import aiohttp
+import urllib3
+from pathlib import Path
+
+from bridge import ZulipNtfyBridge
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+if getattr(sys, 'frozen', False):
+    BASE_DIR = Path(sys.executable).resolve().parent
+else:
+    BASE_DIR = Path(__file__).resolve().parent
+
+ZULIPRC_PATH = BASE_DIR / "zuliprc"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
