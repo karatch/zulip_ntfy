c59bc0373b4b49c300ff4e00651d087dd269e19c
Alexey Karachentsev <alexey.karachentsev@vniira.spb.su>
2026-09-16 09:41:47 +0300

    worked with bridge

--- bridge.py
+++ bridge.py
@@ -6,3 +6,13 @@ from pathlib import Path
 # import database
 
 # Фоновый мост Zulip -> ntfy
+
+class ZulipNtfyBridge:
+    def __init__(self, zulip_site: str, loop: asyncio.AbstractEventLoop, zuliprc_path: Path):
+        self.zulip_site = zulip_site
+        self.loop = loop
+        self.zuliprc_path = zuliprc_path
+        self.bot_email = None
+        self.zulip_client = None
+        self.session = None
+        self.semaphore = asyncio.Semaphore(10)
